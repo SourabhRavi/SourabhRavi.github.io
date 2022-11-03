2a2aadeff0a46b55417b2a318a58cfc6efbbd468
sourabh kumar <sourabh.kumar@petpooja.com>
2022-11-03 23:58:02 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e656506693f5715a6535.js"
+    "url": "webpack-runtime-1aaeafd3f5dc1ecdf20e.js"
   },
   {
     "url": "framework-fbf2011d6c2b83ba228a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "878b3033ca66ee92c98c9888e2fa936d"
+    "revision": "08aba3a8e570b04216bb226c4708bf13"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-27c66d5dcd0cba7d37a1.js"
